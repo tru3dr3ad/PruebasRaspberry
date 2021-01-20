bd9fb5223c87113858b77898cf581a7c14881455
DESKTOP-D9SUL8A\krist <kristal.rojas01@gmail.com>
2021-01-20 17:51:06 -0300

    archivo reemplazado codigo boton luz

--- pruebaluces.py
+++ pruebaluces.py
@@ -1,12 +1,27 @@
 import RPi.GPIO as GPIO
+import time
+
 try:
     GPIO.setmode(GPIO.BCM)
+
+    # Pin 19 will sense for button pushing
+    button = 19
+    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # The LED
     led = 21
     GPIO.setup(led, GPIO.OUT)
-    # Switch on
-    GPIO.output(led, 1)
-    # Switch off
-    GPIO.output(led, 0)
+
+    while True:
+        input_state = GPIO.input(button) # Sense the button
+        if input_state == False:
+            print('Button Pressed')
+            time.sleep(0.2)
+            # Switch on LED
+            GPIO.output(led, 1)
+        else :
+            # Switch off LED
+            GPIO.output(led, 0)
 finally:
-       print("clean up") 
+       print("Limpieza") 
        GPIO.cleanup() # cleanup all GPIO 
