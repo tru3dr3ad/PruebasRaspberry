4aa7ddebba8d7d1f24a7bed65107c553dcd9b30b
DESKTOP-D9SUL8A\krist <kristal.rojas01@gmail.com>
2021-01-20 19:01:26 -0300

    preder luz intento

--- pruebaluces.py
+++ pruebaluces.py
@@ -1,27 +1,13 @@
 import RPi.GPIO as GPIO
-import time
 
 try:
     GPIO.setmode(GPIO.BCM)
-
-    # Pin 19 will sense for button pushing
-    button = 19
-    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    # The LED
     led = 21
     GPIO.setup(led, GPIO.OUT)
-
-    while True:
-        input_state = GPIO.input(button) # Sense the button
-        if input_state == False:
-            print('Button Pressed')
-            time.sleep(0.2)
-            # Switch on LED
-            GPIO.output(led, 1)
-        else :
-            # Switch off LED
-            GPIO.output(led, 0)
+    # Switch on
+    GPIO.output(led, 1)
+    # Switch off
+    GPIO.output(led, 0)
 finally:
-       print("Limpieza") 
+       print("clean up") 
        GPIO.cleanup() # cleanup all GPIO 
