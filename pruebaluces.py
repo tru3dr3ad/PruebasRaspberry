eb5c6c44459564562b76d62039946f84d0175b46
Kristal <kristal_rojas10331@hotmail.com>
2021-01-20 19:42:48 +0000

    Se agrega finally statement

--- pruebaluces.py
+++ pruebaluces.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-led = 21
-GPIO.setup(led, GPIO.OUT)
-# Switch on
-GPIO.output(led, 1)
-# Switch off
-GPIO.output(led, 0)
+try:
+    GPIO.setmode(GPIO.BCM)
+    led = 21
+    GPIO.setup(led, GPIO.OUT)
+    # Switch on
+    GPIO.output(led, 1)
+    # Switch off
+    GPIO.output(led, 0)
+finally:
+       print("clean up") 
+          GPIO.cleanup() # cleanup all GPIO 
